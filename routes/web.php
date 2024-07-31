1b3500dbaaa2806fbeca7477c7f815d8476f0249
Minh Le <minh.leduc.lt@gmail.com>
2024-07-31 15:08:37 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -94,9 +94,6 @@ Route::resource('/comment','PostCommentController');
 // Coupon
 Route::post('/coupon-store','CouponController@couponStore')->name('coupon-store');
 // Payment
-// Route::get('payment', 'PayPalController@payment')->name('payment');
-// Route::get('cancel', 'PayPalController@cancel')->name('payment.cancel');
-// Route::get('payment/success', 'PayPalController@success')->name('payment.success');
 Route::get('/momo-return', 'OrderController@momoReturn');
 Route::get('/vnpay-return', 'OrderController@vnpayReturn');
 Route::post('/order/cancel/{id}','OrderController@cancelOrder')->name('order.cancel');
